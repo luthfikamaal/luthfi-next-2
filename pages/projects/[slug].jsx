3b90728c1c88e4f5baf7a5c3e4f6268b9b0156e5
luthfikamaal <luthfikamal13@gmail.com>
2023-02-08 20:30:17 +0700

    add: detail project's page

--- pages/projects/[slug].jsx
+++ pages/projects/[slug].jsx
@@ -9,7 +9,27 @@ const ShowProject = ({ project }) => {
   return (
     <>
       <Layout title={project.name}>
-        <h1 className="text-lg">{project.name}</h1>
+        <div className="mt-16">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+            <div className="w-full">
+              <img src={project.img} alt={project.name} className="w-full rounded-xl" />
+            </div>
+            <div>
+              <span className="bg-primary text-white px-2 py-1 rounded-lg">{project.type}</span>
+              <h1 className="text-2xl mt-3 text-white">{project.name}</h1>
+              <p className="text-white mb-2">{project.description}</p>
+              <span className="bg-white px-2 py-1 text-secondary rounded-lg">{project.stacks}</span>
+              <div className="flex gap-x-2 mt-2">
+                <a href={project.url.github} target="_blank" className="px-2 py-1 rounded-lg bg-black text-white">
+                  <i className="bi bi-github"></i>
+                </a>
+                <a href={project.url.website} target="_blank" className="px-2 py-1 rounded-lg bg-primary text-white">
+                  <i className="bi bi-globe2"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );
