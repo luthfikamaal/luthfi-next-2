3606a5a0126cd471df806c6306b32ca66a7ae6ff
luthfikamaal <luthfikamal13@gmail.com>
2023-02-09 08:54:16 +0700

    edit: contact page

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -3,12 +3,12 @@ import Layout from '../layouts/Layout';
 const Contact = () => {
   return (
     <>
-      <Layout title={'Contact'} description={'get in touch with me'} thumbnail={'https://luthfikamal-2.vercel.app/assets/thumbnails/contact.png'}>
+      <Layout title={'Contact'} description={"let's get in touch with me"} thumbnail={'https://luthfikamal-2.vercel.app/assets/thumbnails/contact.png'}>
         <div className="mt-16">
           <div className="mb-4">
             <h1 className="text-4xl mb-1 font-semibold text-white">Contact</h1>
             <p className="text-white">
-              get in <span className="font-bold">touch</span> with me
+              let's get in <span className="font-bold">touch</span> with me
             </p>
           </div>
           <hr />
